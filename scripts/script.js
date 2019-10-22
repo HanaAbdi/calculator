4b11a9600d80552239f8cfd76618d73db6c8dfb9
Muhammad Ahmad <muhammadahmad504@gmial.com>
2019-10-22 22:54:38 +0500

    Calculator working for single calculation

--- scripts/script.js
+++ scripts/script.js
@@ -20,7 +20,7 @@ function operate(operator, a, b) {
             return add(a, b);
         case "-":
             return subtract(a, b);
-        case "*":
+        case "x":
             return multiply(a, b);
         case "/":
             return divide(a, b);
@@ -32,8 +32,32 @@ const numbers = Array.from(document.querySelectorAll('button.number'));
 
 let displayValue = "";
 numbers.forEach((number) => {
-    number.addEventListener(('click'), (e) => {
-        displayValue += e.target.innerText;
-        display.innerText = displayValue;
+    number.addEventListener(('click'), updateDisplay);
+});
+
+function updateDisplay(e) {
+    displayValue += e.target.innerText;
+    display.innerText = displayValue;
+} 
+
+let firstValue = "";
+let operation = "";
+let secondValue = "";
+
+const operators = Array.from(document.querySelectorAll('button.operator'));
+operators.forEach((operator) => {
+    operator.addEventListener(('click'), (e) => {
+        firstValue = displayValue;
+        operation = e.target.innerText;
+        // display.innerText = "";
+        displayValue = "";
     });
+});
+
+let result = "";
+const equal = document.querySelector('button.eq');
+equal.addEventListener(('click'), (e) => {
+    secondValue = displayValue;
+    display.innerText = operate(operation, firstValue, secondValue);
+    displayValue = "";
 });
